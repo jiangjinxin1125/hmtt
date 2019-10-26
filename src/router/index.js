b16e024fdacab4020bd4ef11a13749243e84a20e
jiangjinxin1125 <2312041201@qq.com>
2019-10-26 21:33:02 +0800

    欢迎页面完成

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,28 @@ import Vue from 'vue'
 
 import Login from '@/views/login'
 
+import Home from '@/views/home'
+
+import Welcome from '@/views/welcome'
+
+import NotFound from '@/views/404'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/login', component: Login }
+    { path: '/login', component: Login },
+    { path: '/', component: Home },
+    {
+      path: '/',
+      component: Home,
+      children: [
+        {
+          path: '/', component: Welcome
+        }
+      ]
+    },
+    { path: '*', component: NotFound }
   ]
 })
 
